d320f248c9ccdc8c09ea58f8a26bf4d39e61826e
basius <pcw12345>
2016-10-18 17:36:32 +0200

    Create ArrayList of data and adapter.
    For arrayAdapter we give custom context, layout of card and also the row of the card(TextView)

--- app/src/main/java/com/example/a19718/listadocartas/MainActivityFragment.java
+++ app/src/main/java/com/example/a19718/listadocartas/MainActivityFragment.java
@@ -1,16 +1,21 @@
 package com.example.a19718.listadocartas;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+    private ArrayList<String> data;
+    private ArrayAdapter<String> adapter;
 
     public MainActivityFragment() {
     }
@@ -21,6 +26,15 @@ public class MainActivityFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
 
         ListView lvCartas = (ListView) view.findViewById(R.id.lvCartas);
+
+        data = new ArrayList<>();
+        adapter = new ArrayAdapter<String>(
+                getContext(),
+                R.layout.layout_carta,
+                R.id.tvCarta,
+                data
+        );
+        lvCartas.setAdapter(adapter);
 
         return view;
     }
