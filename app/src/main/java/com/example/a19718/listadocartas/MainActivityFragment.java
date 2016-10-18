f1151bc258d22a0ce51a5b6f75cc48a36c0ca3af
basius <pcw12345>
2016-10-18 17:25:35 +0200

    Create new linear layout and horizontal with a list view for the cards

--- app/src/main/java/com/example/a19718/listadocartas/MainActivityFragment.java
+++ app/src/main/java/com/example/a19718/listadocartas/MainActivityFragment.java
@@ -18,11 +18,10 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        //return inflater.inflate(R.layout.fragment_main, container, false);
         View view = inflater.inflate(R.layout.fragment_main, container, false);
 
-                       ListView lvCartas = (ListView) view.findViewById(R.id.lvCartas);
+        ListView lvCartas = (ListView) view.findViewById(R.id.lvCartas);
 
-                       return view;
+        return view;
     }
 }
