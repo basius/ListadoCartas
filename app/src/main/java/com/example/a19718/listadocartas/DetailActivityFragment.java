344a41c57d7ad404017896fb757fc659873f4055
basius <jorabada@gmail.com>
2016-12-02 12:35:45 +0100

    Agafem les dades en el Fragment de detail i les mostrem

--- app/src/main/java/com/example/a19718/listadocartas/DetailActivityFragment.java
+++ app/src/main/java/com/example/a19718/listadocartas/DetailActivityFragment.java
@@ -1,7 +1,9 @@
 package com.example.a19718.listadocartas;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +19,21 @@ public class DetailActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_detail, container, false);
+        View view = inflater.inflate(R.layout.fragment_detail, container, false);
+
+        Intent i = getActivity().getIntent();
+
+        if (i != null) {
+            Card card = (Card) i.getSerializableExtra("card");
+            if (card != null) {
+                updateUi(card);
+            }
+        }
+        return view;
+    }
+
+    private void updateUi(Card card) {
+        Log.d("Card", card.toString());
     }
 }
+
