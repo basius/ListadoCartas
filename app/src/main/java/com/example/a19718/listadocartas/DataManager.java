e92e78791792ed5e47102eeb7daefecc4bcfbb1f
basius <jorabada@gmail.com>
2016-12-20 00:22:26 +0100

    Creem classe DataManager per centralitzar les operacions. S'han cambiat Arraylist per List per conflictes.

--- app/src/main/java/com/example/a19718/listadocartas/DataManager.java
+++ app/src/main/java/com/example/a19718/listadocartas/DataManager.java
@@ -3,6 +3,8 @@ package com.example.a19718.listadocartas;
 import android.content.Context;
 import android.net.Uri;
 import java.util.ArrayList;
+import java.util.List;
+
 import nl.littlerobots.cupboard.tools.provider.UriHelper;
 import static nl.qbusict.cupboard.CupboardFactory.cupboard;
 
@@ -14,7 +16,7 @@ public class DataManager {
     private static UriHelper URI_HELPER = UriHelper.with(CardsContentProvider.AUTHORITY);
         private static Uri CARD_URI = URI_HELPER.getUri(Card.class);
 
-                static void saveCards(ArrayList<Card> movies, Context context) {
+                static void saveCards(List<Card> movies, Context context) {
                 cupboard().withContext(context).put(CARD_URI, Card.class, movies);
            }
 }
